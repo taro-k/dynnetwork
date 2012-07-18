2e28568df74c6e6502f2a8d16cf73f0889282a3c
Sabina Sara Pfister <xanibas@gmail.com>
2012-07-18 17:06:40 +0200

    remove obolete code

--- DynNetwork/src/main/java/org/cytoscape/dyn/internal/view/model/DynNetworkView.java
+++ DynNetwork/src/main/java/org/cytoscape/dyn/internal/view/model/DynNetworkView.java
@@ -130,78 +130,12 @@ public interface DynNetworkView<T>
 	 * @param value
 	 */
 	public void writeLockedVisualProperty(CyEdge edge, VisualProperty<Double> vp, double value);
-
-//	/**
-//	 * Insert node position X.
-//	 * @param node
-//	 * @param interval
-//	 */
-//	public void insertNodePositionX(CyNode node, DynInterval<T> interval);
-//	
-//	/**
-//	 * Insert node position Y.
-//	 * @param node
-//	 * @param interval
-//	 */
-//	public void insertNodePositionY(CyNode node, DynInterval<T> interval);
-//	
-//	/**
-//	 * Insert node position Z.
-//	 * @param node
-//	 * @param interval
-//	 */
-//	public void insertNodePositionZ(CyNode node, DynInterval<T> interval);
-//
-//	/**
-//	 * Remove intervals and attributes belonging to node.
-//	 * @param node
-//	 */
-//	public void removeNode(CyNode node);
-//	
-//	/**
-//	 * Remove all node position intervals.
-//	 */
-//	public void removeAllIntervals();
-//
-//	/**
-//	 * Get all position intervals for node.
-//	 * @param node
-//	 * @return
-//	 */
-//	public List<DynInterval<T>> getIntervals(CyNode node);
-//
-//	/**
-//	 * Search all positions of visible nodes
-//	 * @param interval
-//	 * @return
-//	 */
-//	public List<DynInterval<T>> searchNodePositions(DynInterval<T> interval);
-//	
-//	/**
-//	 * Search all positions of visible nodes that changed from the last time interval.
-//	 * @param interval
-//	 * @return
-//	 */
-//	public List<DynInterval<T>> searchChangedNodePositions(DynInterval<T> interval);
-//
-//	/**
-//	 * Search positions of not visible nodes.
-//	 * @param interval
-//	 * @return
-//	 */
-//	public List<DynInterval<T>> searchNodePositionsNot(DynInterval<T> interval);
 	
 	/**
 	 * Initialize transparency values.
 	 * @param visibility
 	 */
 	public void initTransparency(int visibility);
-	
-//	/**
-//	 * Initialize node positions.
-//	 * @param time
-//	 */
-//	public void initNodePositions(double time);
 	
 	/**
 	 * Get network view.
